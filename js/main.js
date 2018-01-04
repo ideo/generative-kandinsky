b936f2873b170b2fa99ee22e98359db52be77cf2
stringertheory <mike.stringer.internet@gmail.com>
2018-01-04 16:02:41 -0600

    first commit

---
+++ js/main.js
@@ -0,0 +1,102 @@
+// http://www.wassilykandinsky.net/work-247.php
+function regenerate(seed) {
+
+  // Sets Math.random to a PRNG initialized using the given explicit seed.
+  if (seed) {
+    Math.seedrandom(seed)
+  }
+  
+  var N_X = 11
+  var N_Y = 11
+  var BACKGROUND_COLOR = Snap.rgb(230, 220, 210)
+  var BIG_CIRCLE_STROKE_WIDTH = 0.5
+  var BIG_CIRCLE_RADIUS = N_X / 2 - 0.5
+  var BIG_CIRCLE_COLOR = Snap.rgb(25, 35, 45)
+  var N_CIRCLES = 15
+  var N_LINES = 5
+  var N_BARS = 2
+
+  var drawBars = function (nBars) {
+    var n_done = 0
+    while (n_done < nBars) {
+      var bar = s.rect(-N_X / 2, N_Y / 2, 2 * N_X, 2).attr({
+        stroke: 'none',
+        fill: chroma.random(),
+        style: 'mix-blend-mode: darken'
+      })
+      bar.transform(
+        Snap.format('r{angle},{x_center},{y_center}', {
+          angle: 360 * Math.random(),
+          x_center: N_X / 2,
+          y_center: N_Y / 2
+        })
+      )
+      n_done += 1
+    }
+  }
+
+  var drawCircles = function (nCircles, maxRadius) {
+    var n_done = 0
+    while (n_done < nCircles) {
+      var color = chroma.random().hex()
+      var x = 2 + (N_X - 4) * Math.random()
+      var y = 2 + (N_Y - 4) * Math.random()
+      var r = Math.pow(2 * Math.random(), 1)
+      var dx = (x - N_X / 2)
+      var dy = (y - N_Y / 2)
+      var dr = Math.sqrt(Math.pow(dx, 2) + Math.pow(dy, 2))
+      if ((dr + r) < maxRadius) {
+        s.circle(x, y, r).attr({
+          stroke: 'none',
+          fill: color,
+          style: 'mix-blend-mode: darken'
+        })
+        n_done += 1
+      }
+    }
+  }
+
+  var drawLines = function (nLines, length, baseWidth, jitterWidth, color) {
+    var n_done = 0
+    while (n_done < nLines) {
+      var x1 = 2 + (N_X - 4) * Math.random()
+      var y1 = 2 + (N_Y - 4) * Math.random()
+      var x2 = 2 + (N_X - 4) * Math.random()
+      var y2 = 2 + (N_Y - 4) * Math.random()
+      var dx = x2 - x1
+      var dy = y2 - y1
+      var dr = Math.sqrt(Math.pow(dx, 2) + Math.pow(dy, 2))
+      if (dr < N_X / length) {
+        var strokeWidth = baseWidth + jitterWidth * Math.random()
+        s.line(x1, y1, x2, y2).attr({
+          stroke: color,
+          strokeWidth: strokeWidth
+        })
+        n_done += 1
+      }
+    }
+  }
+  
+  var s = makeSVG(N_X, N_Y)
+
+  // background square
+  s.rect(-1, -1, N_X + 2, N_Y + 2).attr({
+    stroke: 'none',
+    fill: BACKGROUND_COLOR,
+    style: 'mix-blend-mode: darken'
+  })
+  
+  // draw big circle
+  s.circle(N_X / 2, N_Y / 2, BIG_CIRCLE_RADIUS).attr({
+    stroke: BIG_CIRCLE_COLOR,
+    fill: 'none',
+    strokeWidth: BIG_CIRCLE_STROKE_WIDTH
+  })
+
+  drawBars(N_BARS)
+  drawCircles(N_CIRCLES, BIG_CIRCLE_RADIUS - BIG_CIRCLE_STROKE_WIDTH)
+  drawLines(N_LINES, 2, 0.01, 0.09, 'black')
+  drawLines(N_LINES, 3, 0.005, 0.015, 'black')
+
+}
+regenerate(window.location.hash)
---
+++ js/utils.js
@@ -0,0 +1,84 @@
+var FRAME_FACTOR = 20
+
+function makeSVG(n_x, n_y) {
+  var s = Snap('#canvas')
+  s.clear()
+  var frame = (n_x + 1) / (FRAME_FACTOR + 1)
+  s.attr({
+    width: 600,
+    height: 600,
+    viewBox: Snap.format('{min_x} {min_y} {width} {height}', {
+      min_x: 0 - frame,
+      min_y: 0 - frame,
+      width: n_x + 2 * frame,
+      height: n_y + 2 * frame
+    })
+  })
+  s.rect(-1, -1, n_x + 2, n_y + 2).attr({
+    // fill: chroma.hcl(90, 1, 100),
+    // fill: chroma.hcl(135, 10, 10),
+    fill: 'none',
+    stroke: 'none'
+  })
+  return s
+}
+
+function dataURItoBlob(dataURI) {
+  // convert base64 to raw binary data held in a string doesn't handle
+  // URLEncoded DataURIs - see SO answer #6850276 for code that does
+  // this
+  var byteString = atob(dataURI.split(',')[1]);
+
+  // separate out the mime component
+  var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0]
+
+  // write the bytes of the string to an ArrayBuffer
+  var ab = new ArrayBuffer(byteString.length);
+  var ia = new Uint8Array(ab);
+  for (var i = 0; i < byteString.length; i++) {
+      ia[i] = byteString.charCodeAt(i);
+  }
+
+  // write the ArrayBuffer to a blob, and you're done
+  var blob = new Blob([ab], {type: mimeString});
+  return blob;
+}
+
+function downloadImages(svg_id) {
+  if (svg_id[0] === '#') {
+    var svg_id = svg_id.split('#')[1];
+  }
+  var svg = document.getElementById(svg_id);
+  svg.toDataURL("image/png", {
+    callback : function(data) {
+      var image = data.replace("image/png", "image/octet-stream");
+
+      var hash = md5(image).substring(0, 6)
+      var filename = 'computer-art-' + svg_id.split('-')[0] + '-' + hash;
+      var png_filename = filename + ".png";
+      var svg_filename = filename + ".svg";
+
+      var blob = dataURItoBlob(image);
+      var blobUrl = URL.createObjectURL(blob);
+      download_as_file(png_filename, blobUrl);
+
+      var svgString = (new XMLSerializer()).serializeToString(svg);
+      var svgUrl = 'data:text/plain;charset=utf-8,';
+      svgUrl += encodeURIComponent(svgString);
+      download_as_file(svg_filename, svgUrl);
+    }
+  });
+}
+
+function download_as_file(filename, url) {
+  console.log(filename);
+  var a = document.createElement('a');
+  a.href = url;
+  a.download = filename;
+  a.click();
+  a.remove();
+}
+
+function jitter(amount) {
+  return amount * (Math.random() - 0.5);
+}
